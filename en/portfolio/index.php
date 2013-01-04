16fabf6ca523f64e02c56f822649ac99b1043350
Jose Viso <jvisov@gmail.com>
2013-01-04 11:46:32 +0100

    portfolio/index (en): Restructuring the index.php

--- en/portfolio/index.php
+++ en/portfolio/index.php
@@ -1,57 +1,76 @@
 <?php
 
- $page_title = "Portfolio";
- $page_description = "Jose Viso's portfolio. Check out the different projects I've work on.";
+ 	$page_title = "Portfolio";
+ 	$page_description = "Jose Viso's portfolio. Check out the different projects I've work on.";
 
- include("../includes/header.php"); ?>
+ 	include("../includes/header.php"); ?>
 
-		
 		<!-- Projects -->	
 		<ul class="thumbnails pull-left">
-		  <li>
-		    <div class="thumbnail">
+			
+		  <li id="ec">
+		    <div class="thumbnail span3">
+		    	<a href="evacriado">
+		    		<img src="../../images/thumb_evaspa.jpg" alt="" width="260" height="197">
+		    		<h3>Centro Eva Criado</h3>
+		    	</a>	
+			</div>
+		  </li>	
+		
+		  <li id="sb">
+		    <div class="thumbnail span3">
 		    	<a href="smartbite">
 		    		<img src="../../images/thumb_smartBite.png" alt="">
 		    		<h3>Smart Bite</h3>
 		    	</a>
-		    	<p>App for eating healthy</p>
 			</div>
 		  </li>
-		  <li>
-		    <div class="thumbnail">
+		
+		  <li id="sr">
+		    <div class="thumbnail span3">
 		    	<a href="streetrockstars">
 		    		<img src="../../images/thumb_streetRockstars.png" alt="">
 		    		<h3>Street Rockstars</h3>
-		    	</a>
-		    	<p>Project to find the best street musicians in the world</p>
+		    	</a>	
 			</div>
 		  </li>
-		  <li>
-		  	<div class="thumbnail">
+		
+		  <li id="ssm">
+		  	<div class="thumbnail span3">
 		  		<a href="supersimple_mobile">
 		    		<img src="../../images/thumb_superSimple_mobile.png" alt="">
 		    		<h3>SuperSimple Mobile</h3>
 		    	</a>
-		    	<p>Concept of a mobile phone for non-tech people</p>
+		    	
 			</div>
 		  </li>
-		  <li>
-		    <div class="thumbnail">
+		
+		  <li id="ne">
+		    <div class="thumbnail span3">
+		    	<a href="nextepisode">
+		    		<img src="http://flickholdr.com/270/205/television" alt="">
+		    		<h3>NextEpisode</h3>
+		    	</a>
+			</div>
+		  </li>
+		
+		  <li id="tmt">
+		    <div class="thumbnail span3">
 		    	<a href="takemethere">
 		    		<img src="../../images/thumb_takeMeThere.png" alt="">
 		    		<h3>Take Me There</h3>
 		    	</a>
-		    	<p>See places as if you were there</p>
 			</div>
 		  </li>
+	
 		</ul>
 
 		<!-- Link to the PDF version of the portfolio -->
 		<div class="row">
 			<div class="centered well">
-				<a href="../portfolio/portfolio_en.pdf"><button class="btn btn-info btn-large" type="button">Download the PDF version</button></a>
+				<a href="../portfolio/portfolio_es.pdf"><button class="btn btn-info btn-large" type="button">Descarga la versión en PDF</button></a>
 			</div>
 		</div>
 
 		
-		<?php include("../includes/footer.php"); ?>
+	<?php include("../includes/footer.php");
