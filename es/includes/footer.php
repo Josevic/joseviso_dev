12c47337b72cdf07c307981bc7c9efeed1b6d44e
Jose Viso <jvisov@gmail.com>
2013-01-16 10:52:04 +0100

    includes/footer (es): Added Bootstrap Lightbox js and animation to scroll to top

--- es/includes/footer.php
+++ es/includes/footer.php
@@ -7,6 +7,7 @@
 
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="<?php echo $base;?>/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo $base;?>/bootstrap-lightbox/bootstrap-lightbox.min.js"></script>
     <script type="text/javascript">
 
 	  var _gaq = _gaq || [];
@@ -18,6 +19,11 @@
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
+
+	  $("a[href='#top']").click(function() {
+		  $("html, body").animate({ scrollTop: 0 }, "slow");
+		  return false;
+		});	
 
 	</script>
   </body>
