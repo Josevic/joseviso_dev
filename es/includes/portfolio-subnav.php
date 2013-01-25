911d75b0ed4ae6d5a16ca1e8417125bb94d54ba1
Jose Viso <jvisov@gmail.com>
2013-01-25 10:25:09 +0100

    includes/portfolio-subnav (es): Added subnav for the portfolio

---
+++ es/includes/portfolio-subnav.php
@@ -0,0 +1,59 @@
+	<div class="row visible-desktop visible-tablet">
+	  <div class="span12 centered-pills test11">
+	    <ul class="nav nav-pills">
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'index.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio'; ?>">Todos</a></li>
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'investigacion-de-usuarios.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio/';?>categoria/investigacion-de-usuarios">Investigación de Usuarios</a></li>		  
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'evaluacion-heuristica.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio/';?>categoria/evaluacion-heuristica">Evaluación Heurística</a></li>
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'diseno-de-interaccion.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio/';?>categoria/diseno-de-interaccion">Diseño de Interacción</a></li>
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'desarrollo.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio/';?>categoria/desarrollo">Desarrollo</a></li>
+	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'fotografia.php')) echo 'class="active"';?>><a href="<?php echo $base . 'es/portfolio/';?>categoria/fotografia">Fotografía</a></li>
+	    </ul>
+	  </div>
+	</div>
+
+	<div class="row visible-phone">
+	  testtt
+	</div>
+
+	<?php
+
+		/* Inclusion of the different projects in each subnav category */
+
+		$projects = array(
+		 			"evacriado" 		 => "Eva Criado",
+		 			"smartbite" 		 => "SmartBite",
+		 			"streetrockstars" 	 => "Street Rockstars",
+		 			"amsterdammers"		 => "Amsterdammers",
+		 			"supersimple_mobile" => "SuperSimple Mobile",
+		 			"nextepisode" 		 => "NextEpisode",
+		 			"takemethere" 		 => "Take Me There"
+		 		);
+		
+		$user_research = array(
+		 			"evacriado" 		 => "Eva Criado",
+		 			"smartbite" 		 => "SmartBite",
+		 			"streetrockstars" 	 => "Street Rockstars",
+		 			"supersimple_mobile" => "SuperSimple Mobile",
+		 			"nextepisode" 		 => "NextEpisode",
+		 			"takemethere" 		 => "Take Me There"
+		 		);
+
+		$heuristic_evaluation = array(
+		 			"evacriado" 		 => "Eva Criado"
+		 		);
+
+		$interaction_design = array(
+		 			"evacriado" 		 => "Eva Criado",
+		 			"smartbite" 		 => "SmartBite",
+		 			"streetrockstars" 	 => "Street Rockstars",
+		 			"supersimple_mobile" => "SuperSimple Mobile",
+		 			"nextepisode" 		 => "NextEpisode",
+		 			"takemethere" 		 => "Take Me There"
+		 		);
+
+		$development = array();
+
+		$photography = array(
+		 			"amsterdammers" 	=> "Amsterdammers"
+		 		);
+	?>
