d50baf08405e2d9929a29b8f8cb70297fcacc705
Jose Viso <jvisov@gmail.com>
2013-02-01 10:56:18 +0100

    about: Added title and some presentation

--- es/sobre-mi.php
+++ es/sobre-mi.php
@@ -5,45 +5,44 @@
 
 	include("includes/header.php"); ?>
 
+	<h1 class="hidden-desktop centered">Sobre mí</h1>
 
+	<div class="row">
+		<div class="span12">
+			<h3>Biografía</h3>
+			<p>Nací en Almería el 29 de Septiembre de 1984, aunque viví en Adra hasta que cumplí los 18 años. Durante el colegio siempre me interesaron las ciencias y sentí una gran pasión por los ordenadores, así que desde bastante pronto tuve claro que quería hacer algo relacionado con la informática.
 
-<div class="row">
-	<div class="span12">
-		<h2>Biografía</h2>
-		<p>Nací en Almería el 29 de Septiembre de 1984, aunque viví en Adra hasta que cumplí los 18 años. Durante el colegio siempre me interesaron las ciencias y sentí una gran pasión por los ordenadores, así que desde bastante pronto tuve claro que quería hacer algo relacionado con la informática.
+			<p>Cuando llegó el momento me fui a Almería a estudiar. Recuerdo esos años como una gran época.</p>
+			<p>En 2007 terminé los estudios y comencé a trabajar como becario en un departamento de sistemas. Fué una buena forma de entrar en el mercado laboral, aunque no me apasionaba lo que hacía. Medio año después comencé a trabajar como desarrollador de software. Durante los siguientes casi 3 años debí ocuparme yo sólo de un proyecto complejo, lo que a la larga creo que ha sido una gran experiencia de aprendizaje sobre muchas cosas que no se enseñan en la universidad.</p>
 
-		<p>Cuando llegó el momento me fui a Almería a estudiar. Recuerdo esos años como una gran época.</p>
-		<p>En 2007 terminé los estudios y comencé a trabajar como becario en un departamento de sistemas. Fué una buena forma de entrar en el mercado laboral, aunque no me apasionaba lo que hacía. Medio año después comencé a trabajar como desarrollador de software. Durante los siguientes casi 3 años debí ocuparme yo sólo de un proyecto complejo, lo que a la larga creo que ha sido una gran experiencia de aprendizaje sobre muchas cosas que no se enseñan en la universidad.</p>
+			<p>Desde siempre había tenido la ilusión de vivir un tiempo en otro país, pero nunca me había terminado de decidir a hacerlo. En el verano de 2010 decidí irme yo sólo a Dublín durante mis vacaciones de verano para intentar mejorar todo lo posible mi inglés.</p>
 
-		<p>Desde siempre había tenido la ilusión de vivir un tiempo en otro país, pero nunca me había terminado de decidir a hacerlo. En el verano de 2010 decidí irme yo sólo a Dublín durante mis vacaciones de verano para intentar mejorar todo lo posible mi inglés.</p>
+			<p>Así que por principios de 2011, con varios años de experiencia laboral, decidí irme a vivir al extranjero. Después de mirar distintas posibilidades, en el mes de Abril me surgió la posibilidad de irme a trabajar a Amsterdam. Así que hice las maletas.</p>
 
-		<p>Así que por principios de 2011, con varios años de experiencia laboral, decidí irme a vivir al extranjero. Después de mirar distintas posibilidades, en el mes de Abril me surgió la posibilidad de irme a trabajar a Amsterdam. Así que hice las maletas.</p>
+			<p>Creo que esa ha sido una de las mejores cosas que he hecho jamás. Descubrí nuevas formas de ver las cosas, nuevas aficiones y perdí cualquier miedo a moverme solo por sitios desconocidos.</p>
 
-		<p>Creo que esa ha sido una de las mejores cosas que he hecho jamás. Descubrí nuevas formas de ver las cosas, nuevas aficiones y perdí cualquier miedo a moverme solo por sitios desconocidos.</p>
+			<p>Durante ese tiempo me di cuenta de que quería enfocar mi carrera hacia facilitar la interacción entre personas y ordenadores. Así que tras un año en el extranjero decidí volver a España para dedicarme completamente a este propósito.</p>
 
-		<p>Durante ese tiempo me di cuenta de que quería enfocar mi carrera hacia facilitar la interacción entre personas y ordenadores. Así que tras un año en el extranjero decidí volver a España para dedicarme completamente a este propósito.</p>
+			<p>Desde entonces trabajo como <em>Diseñador de Experiencia de Usuario</em>, aprendiendo y experimentando sobre los temas que más me interesan de este campo. Continuará.</p>
 
-		<p>Desde entonces trabajo como <em>Diseñador de Experiencia de Usuario</em>, aprendiendo y experimentando sobre los temas que más me interesan de este campo. Continuará.</p>
+			<h3 class="spacer">Estudios</h3>
+			<p>Estudié <em>Ingeniería Técnica en Informática de Gestión</em> en la <a href="http://www.ual.es/">Universidad de Almería</a>. Durante la carrera mis asignaturas favoritas eran las relacionadas con el Diseño de Software y la Interfaz de Usuario.</p> Una vez terminé de estudiar, comencé a interesarme por el campo de la <a href="http://es.wikipedia.org/wiki/Usabilidad">Usabilidad</a>, a leer blogs sobre el tema y más tarde, a intentar poner en práctica estos principios en mi trabajo diario. A continuación empecé a leer más y más libros sobre eso, y fui interesándome progresivamente en las áreas de la <a href="http://es.wikipedia.org/wiki/Arquitectura_de_la_informaci%C3%B3n">Arquitectura de la Información</a> y el <a href="http://es.wikipedia.org/wiki/Dise%C3%B1o_de_interacci%C3%B3n">Diseño de Interacción</a> hasta llegar a cubrir lo que es la <a href="http://es.wikipedia.org/wiki/Experiencia_de_usuario">Experiencia de Usuario</a> y la <a href="http://es.wikipedia.org/wiki/Interacci%C3%B3n_persona-computador">Interacción Persona-Ordenador</a>.</p> 		
 
-		<h2 class="spacer">Estudios</h2>
-		<p>Estudié <em>Ingeniería Técnica en Informática de Gestión</em> en la <a href="http://www.ual.es/">Universidad de Almería</a>. Durante la carrera mis asignaturas favoritas eran las relacionadas con el Diseño de Software y la Interfaz de Usuario.</p> Una vez terminé de estudiar, comencé a interesarme por el campo de la <a href="http://es.wikipedia.org/wiki/Usabilidad">Usabilidad</a>, a leer blogs sobre el tema y más tarde, a intentar poner en práctica estos principios en mi trabajo diario. A continuación empecé a leer más y más libros sobre eso, y fui interesándome progresivamente en las áreas de la <a href="http://es.wikipedia.org/wiki/Arquitectura_de_la_informaci%C3%B3n">Arquitectura de la Información</a> y el <a href="http://es.wikipedia.org/wiki/Dise%C3%B1o_de_interacci%C3%B3n">Diseño de Interacción</a> hasta llegar a cubrir lo que es la <a href="http://es.wikipedia.org/wiki/Experiencia_de_usuario">Experiencia de Usuario</a> y la <a href="http://es.wikipedia.org/wiki/Interacci%C3%B3n_persona-computador">Interacción Persona-Ordenador</a>.</p> 		
+			<p>Durante el verano de 2012 tuve la suerte de participar en la primera edición del magnífico <a href="https://class.coursera.org/hci/class/index"><em>curso online de Interacción Persona-Ordenador</em></a> ofrecido por <a href="https://www.coursera.org">Coursera</a> y la <a href="http://www.stanford.edu/">Universidad de Stanford</a>, y actualmente colaboro en él como Community TA.</p>
 
-		<p>Durante el verano de 2012 tuve la suerte de participar en la primera edición del magnífico <a href="https://class.coursera.org/hci/class/index"><em>curso online de Interacción Persona-Ordenador</em></a> ofrecido por <a href="https://www.coursera.org">Coursera</a> y la <a href="http://www.stanford.edu/">Universidad de Stanford</a>, y actualmente colaboro en él como Community TA.</p>
+			<h3 class="spacer">Personal</h3>
+			
+			<p>Me atraen temas bastante diversos: la tecnología, los deportes, los idiomas, la fotografía, etc. y siempre intento aprender cosas nuevas.</p>
+			
+			<p>También soy un apasionado de la ciencia. Intento ser escéptico y aplicar el método científico en mi forma de pensar. Un libro que me marcó bastante y si alguna vez tengo un hijo recomendaría leer es <a href="http://es.wikipedia.org/wiki/El_mundo_y_sus_demonios"><em>"El mundo y sus demonios"</em></a>, de <a href="http://es.wikipedia.org/wiki/Carl_Sagan">Carl Sagan</a>.</p>
 
-		<h2 class="spacer">Personal</h2>
-		
-		<p>Me atraen temas bastante diversos: la tecnología, los deportes, los idiomas, la fotografía, etc. y siempre intento aprender cosas nuevas.</p>
-		
-		<p>También soy un apasionado de la ciencia. Intento ser escéptico y aplicar el método científico en mi forma de pensar. Un libro que me marcó bastante y si alguna vez tengo un hijo recomendaría leer es <a href="http://es.wikipedia.org/wiki/El_mundo_y_sus_demonios"><em>"El mundo y sus demonios"</em></a>, de <a href="http://es.wikipedia.org/wiki/Carl_Sagan">Carl Sagan</a>.</p>
+			<p>Sigo bastante todo lo referente a la exploración espacial, me parece un tema apasionante. Cuando era niño, trabajar en la NASA era mi trabajo de ensueño.</p>
 
-		<p>Sigo bastante todo lo referente a la exploración espacial, me parece un tema apasionante. Cuando era niño, trabajar en la NASA era mi trabajo de ensueño.</p>
+			<p>Me gusta mucho la esencia del fútbol. Es una de las pocas cosas que existen en el mundo capaces de inspirar a un gran número de gente, junto con la música. Mi equipo es la <a href="http://www.udalmeriasad.com/">Unión Deportiva Almería</a>. 
 
-		<p>Me gusta mucho la esencia del fútbol. Es una de las pocas cosas que existen en el mundo capaces de inspirar a un gran número de gente, junto con la música. Mi equipo es la <a href="http://www.udalmeriasad.com/">Unión Deportiva Almería</a>. 
-
-		<p>Si has llegado hasta aquí y todavía quieres saber algo más de mí (¿en serio?) a continuación tienes las cuentas en redes sociales que más uso:</p>
-
+			<p>Si has llegado hasta aquí y todavía quieres saber algo más de mí (¿en serio?) a continuación tienes las cuentas en redes sociales que más uso:</p>
+		</div>
 	</div>
-</div>
 
 		<?php 
 			include("includes/social_networks.php"); 
