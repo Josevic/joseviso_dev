f6baa8b8d8b60e6d8b699d0666e636aad063b257
Jose Viso <jvisov@gmail.com>
2013-02-08 11:35:24 +0100

    Remove hr and hide for phone

--- en/includes/portfolio_extra.php
+++ en/includes/portfolio_extra.php
@@ -1,7 +1,7 @@
-		<hr/>
+		
 
 		<!-- Indicator of location inside Portfolio -->
-		<ul class="breadcrumb">
+		<ul class="breadcrumb hidden-phone spacer">
 		  <li><a href="<?php echo $base; ?>">Home</a> <span class="divider">/</span></li>
 		  <li><a href="../portfolio">Portfolio</a> <span class="divider">/</span></li>
 		  <li class="active"><?php echo $project_name; ?></li>
