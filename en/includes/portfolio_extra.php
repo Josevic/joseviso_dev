8d60647fdbc9046db6d091d036ce08b787963a2d
Jose Viso <jvisov@gmail.com>
2013-02-08 10:38:32 +0100

    includes/portfolio_extra (en): Added link to home in the breadcrumb

--- en/includes/portfolio_extra.php
+++ en/includes/portfolio_extra.php
@@ -2,7 +2,7 @@
 
 		<!-- Indicator of location inside Portfolio -->
 		<ul class="breadcrumb">
-		  <!-- <li><a href="#">Home</a> <span class="divider">/</span></li> -->
+		  <li><a href="<?php echo $base; ?>">Home</a> <span class="divider">/</span></li>
 		  <li><a href="../portfolio">Portfolio</a> <span class="divider">/</span></li>
 		  <li class="active"><?php echo $project_name; ?></li>
 		</ul>
