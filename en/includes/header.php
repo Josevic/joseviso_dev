efb2b395a509f97c213c49d0439d11ff6ddf1905
Jose Viso <jvisov@gmail.com>
2013-02-13 19:10:06 +0100

    includes/header (en): Added sticky footer styles

--- en/includes/header.php
+++ en/includes/header.php
@@ -19,13 +19,63 @@
 
     <!-- Bootstrap -->
     <link href="<?php echo $base;?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+    /* Sticky footer styles
+      -------------------------------------------------- */
+
+      html,
+      body {
+        height: 100%;
+        /* The html and body elements cannot have any padding or margin. */
+      }
+
+      /* Wrapper for page content to push down footer */
+      #wrap {
+        min-height: 100%;
+        height: auto !important;
+        height: 100%;
+        /* Negative indent footer by it's height */
+        margin: 0 auto -60px;
+      }
+
+      /* Set the fixed height of the footer here */
+      #push,
+      #footer {
+        height: 60px;
+      }
+      #footer {
+        background-color: #f5f5f5; 
+      }
+
+      /* Lastly, apply responsive CSS fixes as necessary */
+      @media (max-width: 767px) {
+        #footer {
+          margin-left: -20px;
+          margin-right: -20px;
+          padding-left: 20px;
+          padding-right: 20px;
+        }
+      }
+
+      /* Custom page CSS
+      -------------------------------------------------- */
+      /* Not required for template or sticky footer method. */
+
+      .container {
+        width: auto;
+        /* max-width: 1200px; */
+      }
+      .container .credit {
+        margin: 20px 0;
+      }
+    </style>
 	<link href="<?php echo $base;?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo $base;?>/bootstrap-lightbox/bootstrap-lightbox.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
   <body>
-
+  <div id="wrap">
   	<div class="navbar navbar-fixed-top">
 	   <div class="navbar-inner">
 	     <div class="container">
@@ -35,12 +85,13 @@
 		        <span class="icon-bar"></span>
 		    </a>
 	       <a class="brand" href="<?php echo $base; ?>">Jose Viso</a>
+
 	       <div class="nav-collapse collapse pull-left visible-phone">
 	       		<a href="<?php echo $base; ?>/es/"><p id="change-language" style="color:black;">En Castellano</p></a></li>
 	       </div>
 		   
 	       <div class="nav-collapse collapse pull-right" id="main-menu">
-		       <ul class="nav" id="main-menu-left">
+		       <ul class="nav">
 		          <li <?php if (strpos($_SERVER['PHP_SELF'], 'about.php')) echo 'class="active"';?>><a href="<?php echo $base;?>/en/about">About</a></li>
 				  <li <?php if (strpos($_SERVER['PHP_SELF'], 'portfolio')) echo 'class="active"';?>><a href="<?php echo $base;?>/en/portfolio">Portfolio</a></li>
 				  <li <?php if (strpos($_SERVER['PHP_SELF'], 'contact.php')) echo 'class="active"';?>><a href="<?php echo $base;?>/en/contact">Contact</a></li>
@@ -52,5 +103,5 @@
 
     <div class="container">
 
-    	<a class="pull-right hidden-phone" href="<?php echo $base; ?>/es/"><p id="change-language" class="muted">En Castellano</p></a>
+    	<a class="pull-right hidden-phone" href="<?php echo $base; ?>/es/"><p id="change-language" class="muted"><small>En Castellano</small></p></a>
 	   
