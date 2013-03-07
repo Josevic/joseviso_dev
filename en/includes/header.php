25af2adc3de8bbd9bd7e58ae35f134317d49ec5a
Jose Viso <jvisov@gmail.com>
2013-03-07 09:43:11 +0100

    includes/header (en): Change the navbar color to inverse and added acute accent

--- en/includes/header.php
+++ en/includes/header.php
@@ -26,7 +26,7 @@
   </head>
 
   <body>
-  	<div class="navbar navbar-fixed-top">
+  	<div class="navbar navbar-inverse navbar-fixed-top">
 	   <div class="navbar-inner">
 	     <div class="container">
 	        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -34,7 +34,7 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		    </a>
-	       <a class="brand" href="<?php echo $base; ?>">Jose Viso</a>   
+	       <a class="brand" href="<?php echo $base; ?>"><b>José Viso</b></a>   
 		   
 	       <div class="nav-collapse collapse" id="main-menu">
 		       <ul class="nav">
