a2a2fc1510a421c1d1a13bae2675121af34b7047
Jose Viso <jvisov@gmail.com>
2013-03-11 17:20:21 +0100

    sobre-mi: Modified to better display the most important info

--- es/sobre-mi.php
+++ es/sobre-mi.php
@@ -16,45 +16,75 @@
 
 	<div class="row">
 		<div class="span12">
-			<h3>Biografía</h3>
-			<p>Nací en Almería el 29 de Septiembre de 1984, aunque viví en Adra hasta que cumplí los 18 años. Durante el colegio siempre me interesaron las ciencias y sentí una gran pasión por los ordenadores, así que desde bastante pronto tuve claro que quería hacer algo relacionado con la informática.
-
-			<p>Cuando llegó el momento me fui a Almería a estudiar. Recuerdo esos años como una gran época.</p>
-
-			<img class="pull-right img-shadow" src="../images/In_Anfield.JPG" style="margin-left:5px; margin-bottom:5px;"/>
 			
-			<p>En 2007 terminé los estudios y comencé a trabajar como becario en un departamento de sistemas. Fué una buena forma de entrar en el mercado laboral, aunque no me apasionaba lo que hacía. Medio año después comencé a trabajar como desarrollador de software. Durante los siguientes casi 3 años debí ocuparme yo sólo de un proyecto complejo, lo que a la larga creo que ha sido una gran experiencia de aprendizaje sobre muchas cosas que no se enseñan en la universidad.</p>
-
-			<p>Desde siempre había tenido la ilusión de vivir un tiempo en otro país, pero nunca me había terminado de decidir a hacerlo. En el verano de 2010 decidí irme yo sólo a Dublín durante mis vacaciones de verano para intentar mejorar todo lo posible mi inglés.</p>
-
-			<p>Así que por principios de 2011, con varios años de experiencia laboral, decidí irme a vivir al extranjero. Después de mirar distintas posibilidades, en el mes de Abril me surgió la posibilidad de irme a trabajar a Amsterdam. Así que hice las maletas.</p>
-
-			<p>Creo que esa ha sido una de las mejores cosas que he hecho jamás. Descubrí nuevas formas de ver las cosas, nuevas aficiones y perdí cualquier miedo a moverme solo por sitios desconocidos.</p>
-
-			<p>Durante ese tiempo me di cuenta de que quería enfocar mi carrera hacia facilitar la interacción entre personas y ordenadores. Así que tras un año en el extranjero decidí volver a España para dedicarme completamente a este propósito.</p>
-
-			<p>Desde entonces trabajo como <em>Diseñador de Experiencia de Usuario</em>, aprendiendo y experimentando sobre los temas que más me interesan de este campo. Continuará.</p>
-
-			<h3 class="spacer">Estudios</h3>
-			<p>Estudié <em>Ingeniería Técnica en Informática de Gestión</em> en la <a href="http://www.ual.es/">Universidad de Almería</a>. Durante la carrera mis asignaturas favoritas eran las relacionadas con el Diseño de Software y la Interfaz de Usuario.</p> Una vez terminé de estudiar, comencé a interesarme por el campo de la <a href="http://es.wikipedia.org/wiki/Usabilidad">Usabilidad</a>, a leer blogs sobre el tema y más tarde, a intentar poner en práctica estos principios en mi trabajo diario. A continuación empecé a leer más y más libros sobre eso, y fui interesándome progresivamente en las áreas de la <a href="http://es.wikipedia.org/wiki/Arquitectura_de_la_informaci%C3%B3n">Arquitectura de la Información</a> y el <a href="http://es.wikipedia.org/wiki/Dise%C3%B1o_de_interacci%C3%B3n">Diseño de Interacción</a> hasta llegar a cubrir lo que es la <a href="http://es.wikipedia.org/wiki/Experiencia_de_usuario">Experiencia de Usuario</a> y la <a href="http://es.wikipedia.org/wiki/Interacci%C3%B3n_persona-computador">Interacción Persona-Ordenador</a>.</p> 		
-
-			<p>Durante el verano de 2012 tuve la suerte de participar en la primera edición del magnífico <a href="https://class.coursera.org/hci/class/index"><em>curso online de Interacción Persona-Ordenador</em></a> ofrecido por <a href="https://www.coursera.org">Coursera</a> y la <a href="http://www.stanford.edu/">Universidad de Stanford</a>, y actualmente colaboro en él como Community TA.</p>
-
-			<h3 class="spacer">Personal</h3>
+			<h3>Quién soy</h3>
+			<div class="row">
+				<div class="offset1 span4">
+					<img src="../images/In_Canal.JPG"/>
+				</div>
+				<div class="span7">
+					<p class="lead">José Viso Vargas</p>
+					<p>29 Septiembre 1984 (28 años)</p>
+					<p>Almería, España</p>
+					<p>Cursando <em><a href="http://www.uoc.edu/masters/esp/web/informatica_multimedia_telecomunicacion/interaccion-persona-ordenador/">Posgrado de Interacción Persona-Ordenador</a> (Universitat Oberta de Catalunya)</em><br/> 
+						<em>Ingeniero Técnico en Informática de Gestión (Universidad de Almería)</em></p>
+				</div>
+			</div>
 			
-			<p>Me atraen temas bastante diversos: la tecnología, los deportes, los idiomas, la fotografía, etc. y siempre intento aprender cosas nuevas.</p>
+			<h3 class="spacer">Qué hago</h3>
+				<div class="row">
+
+					
+					<div class="offset1 span11">
+						<!-- Explanation of what I do -->
+						<p class="lead">Diseño Centrado en el Usuario </p>
+						<p>Mi principal objetivo es diseñar software que satisfaga las necesidades de los usuarios, estudiando sus capacidades, motivaciones y limitaciones, pero teniendo en cuenta también las necesidades del cliente.</p>
+						<p>He trabajado en proyectos complejos tanto en España como en el extranjero, analizando los requisitos de los usuarios y desarrollando software. Más tarde me he especializado en el campo de la Interacción Persona-Ordenador, que es lo que realmente me apasiona. Creo que esta experiencia me permite poseer una buena visión global de los proyectos.</p>
+
+						<h5>Técnicas que utilizo</h5>
+						<ul>
+							<li>Evaluación Heurística</li>
+							<li>Investigación de usuarios</li>
+							<li>Prototipado (papel, baja y alta fidelidad)</li>
+							<li>Wireframing</li>
+							<li>Tests de usuarios</li>
+							<li>Diseño iterativo: Diseño - Prototipado - Evaluación</li>
+						</ul>
+
+						<h5>Proyectos destacados en los que trabajo</h5>
+						<ul>
+							<li><em>Community TA</em> en el <a href="https://www.coursera.org/course/hci">curso de Interacción Persona-Ordenador</a> de <a href="https://www.coursera.org/">Coursera</a> - Stanford University</li>
+							<li><a href="http://streetrockstars.com">Street Rockstars</a></li>
+						</ul>
+
+						<h5>Experiencia</h5>
+						<ul>
+							<li>Consultor de Experiencia de Usuario</li>
+							<li>Desarrollador web (Ámsterdam, Países Bajos)</li>
+							<li>Desarrollador backend (Almería, España)</li>
+						</ul>
+					</div>
+
 			
-			<p>También soy un apasionado de la ciencia. Intento ser escéptico y aplicar el método científico en mi forma de pensar. Un libro que me marcó bastante y si alguna vez tengo un hijo recomendaría leer es <a href="http://es.wikipedia.org/wiki/El_mundo_y_sus_demonios"><em>"El mundo y sus demonios"</em></a>, de <a href="http://es.wikipedia.org/wiki/Carl_Sagan">Carl Sagan</a>.</p>
 
-			<p>Sigo bastante todo lo referente a la exploración espacial, me parece un tema apasionante. Cuando era niño, trabajar en la NASA era mi trabajo de ensueño.</p>
+				</div>
+			
+			<h3 class="spacer">Cosas que me gustan</h3>
+				<div class="row">
+					<div class="offset1 span3">
+						
+						<ul>
+							<li>Deportes</li>
+							<li>Fotografía</li>
+							<li>Música</li>
+							<li>Ciencia</li>
+							<li>Viajar</li>
+						</ul>
+					</div>
 
-			<p>Me gusta mucho la esencia del fútbol. Es una de las pocas cosas que existen en el mundo capaces de inspirar a un gran número de gente, junto con la música. Mi equipo es la <a href="http://www.udalmeriasad.com/">Unión Deportiva Almería</a>. 
-
-			<p>Si has llegado hasta aquí y todavía quieres saber algo más de mí (¿en serio?) a continuación tienes las cuentas en redes sociales que más uso:</p>
-		</div>
+				</div>
+			
+			</div>
 	</div>
 
-		<?php 
-			include("includes/social_networks.php"); 
-			include("includes/footer.php"); 
-	?>
+		<?php include("includes/footer.php"); ?>
