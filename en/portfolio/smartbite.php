4eeffe30ae65f57992cbc293766d7996b3f59176
Jose Viso <jvisov@gmail.com>
2013-05-14 12:27:24 +0200

    portfolio/smartbite (en): Refactoring some parts.

--- en/portfolio/smartbite.php
+++ en/portfolio/smartbite.php
@@ -2,57 +2,31 @@
 
 	$page_title = "Portfolio > SmartBite";
 	$page_description = "Jose Viso's portfolio | Smartbite: An app for eating healthy.";
+	$project_name = "SmartBite";
+	$herounit_width = 7;
+	$herounit_desc = "<p><em>SmartBite</em> is a diet application for smartphones focused on motivate the users to not give in, adapting the diet to the favourite food of each person and providing a wide range of meal alternatives.</p> 
 
-	include("../includes/header.php"); ?>
-
-	<div class="row">
-		<div class="span12">
-			<ul class="breadcrumb">
-				<li><a href="<?php echo $base; ?>">Home</a> <span class="divider">></span></li>
-				<li><a href="<?php echo $base; ?>/en/portfolio"> Portfolio</a> <span class="divider">></span></li>
-				<li class="active">SmartBite</li>	
-		    </ul>
-		</div>
-	</div>
-
-		<!-- Big Text -->	
-		<div class="header"><h1>SmartBite</h1></div>
-		
-		<!-- Summary -->
-		<div class="row spacer">
-			<div class="hero-unit span7">
-				<p><em>SmartBite</em> is a diet application for smartphones focused on motivate the users to not give in, adapting the diet to the favourite food of each person and providing a wide range of meal alternatives.</p> 
-
-				<p>Created during the <a href="https://class.coursera.org/hci/class/index"><em>Human-Computer Interaction course</em></a> offered by <a href="https://www.coursera.org/stanford">Coursera - Stanford University</a>.</p>
+				<p>Created during the <a href='https://class.coursera.org/hci/class/index'><em>Human-Computer Interaction course</em></a> offered by <a href='https://www.coursera.org/stanford'>Coursera - Stanford University</a>.</p>
 				<br/>
-				<a class="btn btn-large btn-primary pull-right" href="https://dl.dropbox.com/u/3953712/SmartBite/test/screens/iPhoneDashboard.html">Try the interactive prototype!</a>
-			</div>
-			<div class="span3 pull-right hidden-phone">
-				<img src="../../images/smartbite.png">
-			</div>
-		</div>
+				<a class='btn btn-large btn-primary pull-right' href='https://dl.dropbox.com/u/3953712/SmartBite/test/screens/iPhoneDashboard.html'>Try the interactive prototype!</a>";
+	
+	$image_front = "../../images/smartbite.png";
 
-		<hr/>
+	$design_opportunity_desc =
+		"<p>The first part of this project was to find not covered needs and design opportunities around me, related to a topic up to me. 
+			I decided to observe and interview <b>people in diet</b> to discover how they knew what food could/should eat and how they got motivated.</p>
 
-		<!-- Design Opportunity -->
-		<div class="row">
-			<div class="span12">
-				<h3>Design opportunity</h3>
-			</div>
-			<div class="span12">
-				<p>The first part of this project was to find not covered needs and design opportunities around me, related to a topic up to me. 
-				   I decided to observe and interview <b>people in diet</b> to discover how they knew what food could/should eat and how they got motivated.</p>
+		  <p>The problems and opportunities found were the following:</p>
+			<ul>
+				<li><p>The <b>lack of motivation</b> for keep following the diet after the first days.</p></li>
+				<li><p>People get tired of having just a <b>few choices for each meal</b>. Eating routine.</p></li>
+				<li><p><b>To know if a given food is appropiate</b> for the diet the person is following.</p></li>
+				<li><p><b>Lack of time</b> to cook. Need of finding specific recipes.</p></li>
+			</ul>";
 
-				<p>The problems and opportunities found were the following:</p>
-				<ul>
-					<li><p>The <b>lack of motivation</b> for keep following the diet after the first days.</p></li>
-					<li><p>People get tired of having just a <b>few choices for each meal</b>. Eating routine.</p></li>
-					<li><p><b>To know if a given food is appropiate</b> for the diet the person is following.</p></li>
-					<li><p><b>Lack of time</b> to cook. Need of finding specific recipes.</p></li>
-				</ul>
-
-			</div>
-		</div>
+	include("../includes/header.php"); 
+	include("../includes/portfolio-template.php");
+	?>
 
 		<hr/>
 
