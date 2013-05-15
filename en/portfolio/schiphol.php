617cd67f4ad45586a1177903c3c5f87917e299e1
Jose Viso <jvisov@gmail.com>
2013-05-15 10:26:18 +0200

    portfolio/schiphol (en): Refactored

--- en/portfolio/schiphol.php
+++ en/portfolio/schiphol.php
@@ -2,30 +2,16 @@
 
 	$page_title = "Portfolio > Schiphol";
 	$page_description = "Jose Viso's portfolio | Schiphol: Amsterdam Airport mobile page.";
+	$project_name = "Schiphol";
+	$herounit_width = 10;
+	$herounit_desc = "<p>I contributed to this project helping to develop the <a href='https://schiphol.mobi'>mobile website</a> and backend system of <a href='http://en.wikipedia.org/wiki/Amsterdam_Airport_Schiphol'>Amsterdam Airport Schiphol</a> (one of Europe's largest), while working for <a href='http://m2mobi.com'>M2Mobi</a>.";
 
-	include("../includes/header.php"); ?>
 
-	<div class="row">
-		<div class="span12">
-			<ul class="breadcrumb">
-				<li><a href="<?php echo $base; ?>">Home</a> <span class="divider">></span></li>
-				<li><a href="<?php echo $base; ?>/en/portfolio"> Portfolio</a> <span class="divider">></span></li>
-				<li class="active">Schiphol</li>	
-		    </ul>
-		</div>
-	</div>
-
-		<!-- Big Text -->	
-		<div class="header"><h1>Schiphol</h1></div>
-		
-		<!-- Summary -->
-		<div class="row spacer">
-			<div class="hero-unit span10">
-				<p>I contributed to this project helping to develop the <a href="https://schiphol.mobi">mobile website</a> and backend system of <a href="http://en.wikipedia.org/wiki/Amsterdam_Airport_Schiphol">Amsterdam Airport Schiphol</a> (one of Europe's largest), while working for <a href="http://m2mobi.com">M2Mobi</a>.</p>
-			</div>
- 		</div>
-
-		<hr/>
+	include("../includes/header.php"); 
+	include("../includes/portfolio-sections/breadcrumb.php");
+	include("../includes/portfolio-sections/project-title.php"); 
+	include("../includes/portfolio-sections/project-summary.php");
+	?>
 
 		<!-- Design Process -->
 		<div class="row">
@@ -75,56 +61,27 @@
 
 		<hr/>
 
-		<!-- Tools Used -->
-		<div class="row">
-			<div class="span12">
-				<h3>Tools and Methodologies used</h3>
-			</div>
-			<div class="offset4 span4">
-				<!-- <img src="http://placehold.it/700x150"> -->
-				<table class="table table-bordered">
-					<tr>
-						<td><p class="centered">PHP</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered">HTML</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered">CSS</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered">Git</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered"><a href="http://ellislab.com/codeigniter">CodeIgniter</a>-based framework</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered">Peer Review</p></td>
-					</tr>
-					<tr>
-						<td><p class="centered">Scrum</p></td>
-					</tr>
-  				</table>
-			</div>
-		</div>
 
-		<hr/>
+	<?php 
 
-		<div class="row">
-			<div class="span12">
-				<h3>Details</h3>
-			</div>
-			<div class="span12">
-				<dl class="dl-horizontal">
-				  <dt>Date</dt>
-				  <dd>April 2011 - April 2012</dd>
-				
-				  <dt>Tags</dt>
-				  <dd>
-				      <a href="<?php echo $base . '/en/portfolio/category/development'; ?>"><span class="label">Development</span></a>
-				  </dd>
-				</dl>
-			</div>
-		</div>
+	$tools_used = array(
+		"PHP",
+		"HTML",
+		"CSS",
+		"Git",
+		"<a href='http://ellislab.com/codeigniter'>CodeIgniter</a>-based framework",
+		"Peer Review",
+		"Scrum"
+	);
 
-	<?php include("../includes/footer.php"); ?>
+	$details = array(
+		"date" => "April 2011 - April 2012",
+		"tags" => array(
+			"Development" => "development"
+		)
+	);
+	include("../includes/portfolio-sections/tools-used.php");
+	include("../includes/portfolio-sections/project-details.php");
+	include("../includes/footer.php");
+
+	?>
