f23f91c9558379fb23ae59eb2ec01d8575eb4e7b
Jose Viso <jvisov@gmail.com>
2013-05-16 19:02:28 +0200

    includes/portfolio-sections/design-opportunity (en): Simplified.

--- en/includes/portfolio-sections/design-opportunity.php
+++ en/includes/portfolio-sections/design-opportunity.php
@@ -2,7 +2,7 @@
 	<?php if (isset($design_opportunity_desc)): ?>
 	<div class="row">
 		<div class="span12">
-			<h3>Design opportunity and strategy</h3>
+			<h3>Design opportunity</h3>
 		</div>
 		<div class="span12">
 			<?php echo $design_opportunity_desc; ?>
