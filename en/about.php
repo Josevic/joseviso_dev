3a11576950bbffada3741f15cce591b7a3cc0c5d
Jose Viso <jvisov@gmail.com>
2013-05-17 20:45:31 +0200

    about (en): Improved section

--- en/about.php
+++ en/about.php
@@ -4,16 +4,12 @@
  	$page_description = "Biography, studies and more personal information.";
 
 	include("includes/header.php"); ?>
-		
-	<!-- <h1 class="hidden-desktop centered">About</h1> -->
-	<div class="row hidden-desktop">
-		<div class="span6">
-			<ul class="breadcrumb">
-				<li><a href="<?php echo $base; ?>">Home</a> <span class="divider">/</span></li>
-				<li class="active">About</li>
-			</ul>
-		</div>
-	</div>
+
+	<header class="jumbotron subhead">
+	  <div class="container">
+	    <h1 style="color:gray">About me</h1>
+	  </div>
+	</header>
 
 	<div class="row">
 		<div class="span12">
@@ -40,30 +36,112 @@
 					<div class="offset1 span11">
 						<!-- Explanation of what I do -->
 						<p class="lead">User Centered Design</p>
-						<p>My main goal is to design software that satisfy the users' needs, studying their capabilities, motivations and limitations, but considering the client's needs too.</p>
+						<p>My main goal is to <strong>design software that satisfy the users' needs</strong>, studying their capabilities, motivations and limitations, but considering the client's needs too.</p>
 						<p>I've worked in complex projects, both in Spain and abroad, analyzing the user requirements and developing software. Later I've specialized in the Human-Computer Interaction field, that is what I'm really passionate about. I think this experience helps me to have a global vision for the projects I work on. 
 						   You can find some of the things I've made taking a look at <a href="portfolio">my portfolio</a>.</p>
 
-						<h5 class="spacer">Techniques I use</h5>
-						<ul>
-							<li>Heuristic Evaluations</li>
-							<li>User Research</li>
-							<li>Personas</li>
-							<li>Scenarios</li>
-							<li>Storyboarding</li>
-							<li>Prototyping (paper, low and high fidelity)</li>
-							<li>Wireframing</li>
-							<li>User tests</li>
-							<li>Iterative Design: (Design - Prototype - Evaluate)</li>
-						</ul>
+						<div class="row">
+							
+							<div class="span4">
+								<h4 class="spacer">Techniques and Methodologies I use</h4>
+								<ol>
+									<li>Sketchboard</li>
+									<li>Heuristic Evaluations and other Usability studies</li>
+									<li>User Research</li>
+									<li>Personas</li>
+									<li>Scenarios</li>
+									<li>Storyboarding</li>
+									<li>Sitemap diagrams</li>
+									<li>User flow diagrams</li>
+									<li>Prototyping (paper prototypes, low and high fidelity graphic tools, code prototypes)</li>
+									<li>Wireframing</li>
+									<li>User tests</li>
+									<li>Iterative Design: (Design - Prototype - Evaluate)</li>
+									<li>Agile Development</li>
+								</ol>
+							</div>
+							<div class="span6">
+								<div id="myCarousel" class="carousel slide">  
+									<ol class="carousel-indicators">
+										<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+										<li data-target="#myCarousel" data-slide-to="1"></li>
+										<li data-target="#myCarousel" data-slide-to="2"></li>
+										<li data-target="#myCarousel" data-slide-to="3"></li>
+										<li data-target="#myCarousel" data-slide-to="4"></li>
+										<li data-target="#myCarousel" data-slide-to="5"></li>
+										<li data-target="#myCarousel" data-slide-to="6"></li>
+										<li data-target="#myCarousel" data-slide-to="7"></li>
+									</ol>
 
-						<h5 class="spacer">Distinguished Projects I'm involved</h5>
+									<!-- Carousel items -->  
+								    <div class="carousel-inner">
+
+								    	<div class="item active">
+								        	<img src="../images/sketchboard.jpg" alt="Sketchboard" />
+								        	<div class="carousel-caption">
+								        		<p>Sketchboard</p>
+								        	</div>
+								        </div> 
+								        
+								        <div class="item ">
+								        	<img src="../images/evacriado_personas.gif" alt="Personas"/>
+								        	<div class="carousel-caption">
+								        		<p>Personas</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/smartbite_storyboard.png" alt="Storyboard"/>
+								        	<div class="carousel-caption">
+								        		<p>Storyboard</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/NextEpisode_paperPrototype.jpg" alt="Paper prototype"/>
+								        	<div class="carousel-caption">
+								        		<p>Paper prototype</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/museoAlmeria_mf-wireframe.jpg" alt="Annotated wireframe"/>
+								        	<div class="carousel-caption">
+								        		<p>Annotated wireframe</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/museoAlmeria_hf-wireframe.jpg" alt="High-fidelity wireframe"/>
+								        	<div class="carousel-caption">
+								        		<p>High-fidelity wireframe</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/user_test.png" alt="User test"/>
+								        	<div class="carousel-caption">
+								        		<p>User test</p>
+								        	</div>
+								        </div> 
+								        <div class="item ">
+								        	<img src="../images/live_website.png" alt="Live website"/>
+								        	<div class="carousel-caption">
+								        		<p>Live website</p>
+								        	</div>
+								        </div> 
+								        
+								  	</div>
+
+								  	<!-- Carousel nav -->  
+								  	<a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a>  
+								  	<a class="carousel-control right" href="#myCarousel" data-slide="next">›</a>  
+								</div>  
+							</div>
+						</div>
+
+						<h4 class="spacer">Distinguished Projects I'm involved</h4>
 						<ul>
 							<li><em>Community TA</em> in the <a href="https://www.coursera.org/course/hci">Human-Computer Interaction course</a> by <a href="https://www.coursera.org/">Coursera</a> - Stanford University</li>
 							<li>Founder of <a href="http://streetrockstars.com">Street Rockstars</a></li>
 						</ul>
 
-						<h5 class="spacer">Experience</h5>
+						<h4 class="spacer">Experience</h4>
 						<ul>
 							<li>User Experience Designer / Developer (Almería, Spain - International)</li>
 							<li>Web Developer (Amsterdam, Netherlands)</li>
@@ -77,15 +155,8 @@
 			
 			<h3 class="spacer">Things I like</h3>
 				<div class="row">
-					<div class="offset1 span3">
-						
-						<ul>
-							<li>Sports</li>
-							<li>Photography</li>
-							<li>Music</li>
-							<li>Science</li>
-							<li>To Travel</li>
-						</ul>
+					<div class="offset1 span11">			
+						<p>Sports, Photography, Music, Science, To travel</p>
 					</div>
 
 				</div>
