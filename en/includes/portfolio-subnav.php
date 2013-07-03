6158c99545d4a2a0d55120c263531f4274521fff
Jose Viso <jvisov@gmail.com>
2013-07-03 10:03:44 +0200

    includes/portfolio-subnav (en): Removed projects

--- en/includes/portfolio-subnav.php
+++ en/includes/portfolio-subnav.php
@@ -19,43 +19,17 @@
 					elseif (strpos($_SERVER['PHP_SELF'], 'development.php')){
 						echo "<h3 class='muted centered'>Development</h3>";
 					}
-					elseif (strpos($_SERVER['PHP_SELF'], 'other.php')){
-						echo "<h3 class='muted centered'>Other</h3>";
-					}
 				?>
 		</div>
 	</div>
 
-	<div class="row visible-desktop visible-tablet">
-	  <div class="span12 centered-pills">
-	    <ul class="nav nav-pills">
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'index.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio'; ?>">All</a></li>
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'user-research.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/user-research">User Research</a></li>		  
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'heuristic-evaluation.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/heuristic-evaluation">Heuristic Evaluation</a></li>
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'interaction-design.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/interaction-design">Interaction Design</a></li>
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'development.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/development">Development</a></li>
-	      <li <?php if (strpos($_SERVER['PHP_SELF'], 'other.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/other">Other</a></li>
-	    </ul>
-	  </div>
-	</div>
-
-
-	<!-- 
-	 <div class="row clearfix visible-phone centered">
-	  	<div class="span12">
-        	<div class="controls">
-              <select name="myselect" onchange="redirectMe(this);">
-                <option value="<?php echo $base . '/en/portfolio'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'index.php')) echo 'selected="selected"';?>>All</option>
-                <option value="<?php echo $base . '/en/portfolio/category/user-research'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'user-research.php')) echo 'selected="selected"';?>>User Research</option>
-                <option value="<?php echo $base . '/en/portfolio/category/heuristic-evaluation'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'heuristic-evaluation.php')) echo 'selected="selected"';?>>Heuristic Evaluation</option>
-                <option value="<?php echo $base . '/en/portfolio/category/interaction-design'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'interaction-design.php')) echo 'selected="selected"';?>>Interaction Design</option>
-                <option value="<?php echo $base . '/en/portfolio/category/development'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'development.php')) echo 'selected="selected"';?>>Development</option>
-                <option value="<?php echo $base . '/en/portfolio/category/other'?>" <?php if (strpos($_SERVER['PHP_SELF'], 'other.php')) echo 'selected="selected"';?>>Other</option>
-              </select>
-            </div>
-        </div>
-      </div>
-	-->	
+	<ul class="nav nav-pills nav-stacked pull-right span3 spacer visible-desktop visible-tablet">
+    	<li <?php if (strpos($_SERVER['PHP_SELF'], 'index.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio'; ?>">All</a></li>
+	    <li <?php if (strpos($_SERVER['PHP_SELF'], 'user-research.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/user-research">User Research</a></li>		  
+	    <li <?php if (strpos($_SERVER['PHP_SELF'], 'heuristic-evaluation.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/heuristic-evaluation">Heuristic Evaluation</a></li>
+	    <li <?php if (strpos($_SERVER['PHP_SELF'], 'interaction-design.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/interaction-design">Interaction Design</a></li>
+	    <li <?php if (strpos($_SERVER['PHP_SELF'], 'development.php')) echo 'class="active"';?>><a href="<?php echo $base . '/en/portfolio/';?>category/development">Development</a></li>
+    </ul>
 
 	<div class="row clearfix visible-phone centered">
 	  	<div class="span12">
@@ -76,7 +50,6 @@
                 <option value="<?php echo $base . '/en/portfolio/category/heuristic-evaluation'?>">Heuristic Evaluation</option>
                 <option value="<?php echo $base . '/en/portfolio/category/interaction-design'?>">Interaction Design</option>
                 <option value="<?php echo $base . '/en/portfolio/category/development'?>">Development</option>
-                <option value="<?php echo $base . '/en/portfolio/category/other'?>">Other</option>
               </select>
             </div>
         </div>
@@ -92,20 +65,13 @@
 		 			"museoalmeria" 		 => "Museum of Almería Kiosk",
 		 			"renfe"				 => "Redesign Renfe.com",
 		 			"schiphol"			 => "Schiphol",
-		 			"streetrockstars" 	 => "Street Rockstars",
-		 			"supersimple_mobile" => "SuperSimple Mobile",
-		 			"nextepisode" 		 => "NextEpisode",
-		 			"takemethere" 		 => "Take Me There",
-		 			"amsterdammers"		 => "Amsterdammers"
+		 			"streetrockstars" 	 => "Street Rockstars"
 		 		);
 		
 		$user_research = array(
 		 			"evacriado" 		 => "Eva Criado",
 		 			"smartbite" 		 => "SmartBite",
 		 			"streetrockstars" 	 => "Street Rockstars",
-		 			"supersimple_mobile" => "SuperSimple Mobile",
-		 			"nextepisode" 		 => "NextEpisode",
-		 			"takemethere" 		 => "Take Me There"
 		 		);
 
 		$heuristic_evaluation = array(
@@ -120,17 +86,10 @@
 		 			"museoalmeria" 		 => "Museum of Almería Kiosk",
 		 			"renfe"				 => "Redesign Renfe.com",
 		 			"streetrockstars" 	 => "Street Rockstars",
-		 			"supersimple_mobile" => "SuperSimple Mobile",
-		 			"nextepisode" 		 => "NextEpisode",
-		 			"takemethere" 		 => "Take Me There"
 		 		);
 
 		$development = array(
 					"schiphol"		  => "Schiphol",
 					"streetrockstars" => "Street Rockstars"
 			);
-
-		$other = array(
-		 			"amsterdammers" 	=> "Amsterdammers"
-		 		);
 	?>
