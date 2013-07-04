8849bbfc77ca6173e9d134a6b8b8e103c825510a
Jose Viso <jvisov@gmail.com>
2013-07-04 12:17:40 +0200

    portfolio (es): Added a better explanation to Smartbite

--- es/portfolio/smartbite.php
+++ es/portfolio/smartbite.php
@@ -20,15 +20,22 @@
 		
 		<!-- Summary -->
 		<div class="row spacer">
-			<div class="hero-unit span7">
-				<p><em>SmartBite</em> es una aplicación de dietas para smartphone enfocada en motivar a los usuarios a no abandonar, adaptándo la dieta a la comida preferida de cada persona y proporcionando una amplia variedad de alternativas.</p>
-				<p>Creado durante el <a href="http://hci-class.org">curso de Interacción-Persona Ordenador</a> ofrecido por <a href="https://www.coursera.org/stanford">Coursera - Stanford University</a>.</p>
-				<br/>
-				<a class="btn btn-large btn-primary pull-right" href="https://dl.dropbox.com/u/3953712/SmartBite/test/screens/iPhoneDashboard.html">¡Prueba el prototipo interactivo!</a>
+			<div class="span12">
+				<div class="row">
+					<div class="span7">
+						<p class="lead"><em>SmartBite</em> es una aplicación de dietas para smartphone enfocada en motivar a los usuarios a no abandonar, adaptándo la dieta a la comida preferida de cada persona y proporcionando una amplia variedad de alternativas.</p>
+						<p class="lead">Creado durante el <a href="http://hci-class.org">curso de Interacción-Persona Ordenador</a> ofrecido por <a href="https://www.coursera.org/stanford">Coursera - Stanford University</a>.</p>
+						<br/>
+						<a class="btn btn-large btn-primary" href="https://dl.dropbox.com/u/3953712/SmartBite/test/screens/iPhoneDashboard.html">¡Prueba el prototipo interactivo!</a>
+					</div>
+					<div class="span5">
+						<div class="pull-right hidden-phone">
+							<img src="../../images/smartbite.jpg">
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="span3 pull-right hidden-phone">
-				<img src="../../images/smartbite.png">
-			</div>
+			
 		</div>
 
 		<hr/>
@@ -36,9 +43,9 @@
 		<!-- Design Opportunity -->
 		<div class="row">
 			<div class="span12">
-				<h3>Oportunidad de diseño</h3>
+				<h3 style="color:gray">Oportunidad de diseño</h3>
 			</div>
-			<div class="span12">
+			<div class="span6">
 				<p>La primera parte de este proyecto fue buscar necesidades no cubiertas u oportunidades de diseño a mi alrededor relacionadas a un tema a mi elección. 
 				   Me decidí por observar y entrevistar a <b>gente que hace dieta</b> y ver cómo decide qué comida puede o debería comer, cómo obtiene motivación y cómo hace para continuar con la dieta.</p>
 
@@ -49,7 +56,10 @@
 					<li><p>Dificultad para <b>saber si un alimento es apropiado</b> para la dieta que se está siguiendo.</p></li>
 					<li><p><b>Falta de tiempo</b> para poder cocinar y necesidad de encontrar recetas específicas.</p></li>
 				</ul>
-
+			</div>
+			<div class="span6">
+				<img src="../../images/smartbite_observation.jpg" alt="" />
+				<p class="centered muted">Una de las personas observadas para encontrar oportunidades de diseño.</p>
 			</div>
 		</div>
 
@@ -58,78 +68,60 @@
 		<!-- Design Process -->
 		<div class="row">
 			<div class="span12">
-				<h3>Proceso seguido</h3>
+				<h3 style="color:gray">Proceso seguido</h3>
 			</div>
-			<div class="span6">
+			<div class="span12">
+				<p>Tras haber detectado una serie de oportunidades de mejora, comencé a diseñar una solución que resolviese dichos problemas. Para ello seguí los siguientes pasos:</p>
 				<ol>
-					<li><p class="spacer">Creación de un <b>punto de vista</b>:</p>
-						 <blockquote><p>Sin apoyo y motivación, las dietas son una lata. Las mejoras de las tecnologías móviles e Internet pueden ser una gran herramienta de ayuda.</p></blockquote></li>
-					<li><p class="spacer">Bocetos de la estructura: uso de <b>storyboards</b>, elección de una dirección concreta y creación de <b>prototipos rápidos</b> con <a href="http://www.balsamiq.com/">Balsamiq Mockups</a>.</p></li>
-					<li><p class="spacer">Creación de <b>prototipos interactivos</b> usando <a href="http://www.justinmind.com/">Justinmind</a>.</p></li>
-					<li><p class="spacer"><b>Pruebas de Usuario</b>: Definición de un Plan de Evaluación, realización de las pruebas, análisis de los resultados y planificación de la siguiente iteración de diseño.</p></li>
+					<li><p class="spacer">Creé un <b>punto de vista</b> para dirigir el proceso en todo momento:</p>
+						 <blockquote><p>Sin apoyo y motivación, las dietas son una lata. Las mejoras de las tecnologías móviles e Internet pueden ser una gran herramienta de ayuda.</p></blockquote>
+					</li>
+
+					<li>
+						<p class="spacer">Elaboré <b>storyboards</b> para representar el contexto de uso de la aplicación: <br/>
+							<div class="centered">
+								<img src="../../images/smartbite_storyboard.png" alt="Storyboard" />
+							</div>
+						</p>
+					</li>
+					<li>
+						<p class="spacer">Realicé bocetos de la estructura, elegí una dirección concreta para continuar desarrollando la idea y creé varios <b>prototipos rápidos</b> alternativos con <a href="http://www.balsamiq.com/">Balsamiq Mockups</a>.<br/>
+							<div class="centered">
+								<img src="../../images/smartbite_lowfidelity2.png" alt="" />
+							</div>
+						</p>
+					</li>
+					<li>
+						<p class="spacer">A continuación realicé y recibí <b>evaluaciones heurísticas</b> sobre dichos prototipos. Con ese feedback realicé las modificaciones correspondientes para mejorarlos.</p>
+					</li>
+					<li>
+						<p class="spacer">Con la seguridad de que estaba en la dirección correcta y se habían solucionado los problemas más graves de usabilidad, creé un <b>prototipo interactivo</b> de más alta fidelidad usando <a href="http://www.justinmind.com/">Justinmind</a>.
+							<div class="centered">
+								<img src="../../images/smartbite_highfidelity.png" alt="" />
+							</div>
+						</p>
+					</li>
+					<li>
+						<p class="spacer">Más adelante elaboré un plan de evaluación, realicé <b>Pruebas de Usuario</b> con 3 personas usando el prototipo creado anteriormente (y otra alternativa de diseño para comparar), analicé los resultados y planifiqué la siguiente iteración de diseño.
+							<div class="centered">
+								<img src="../../images/smartbite_usertest.jpg" alt="" />
+							</div>
+						</p>
+					</li>
+					<li>
+						<p class="spacer">Por último incorporé los cambios motivados por las pruebas de usuario en la aplicación. El más significativo fue el simplificar el proceso de registro.<br/>(<a href="https://dl.dropbox.com/u/3953712/SmartBite/test/screens/iPhoneDashboard.html">Prototipo interactivo</a> modificado tras ese paso).</p>
+					</li>
 				</ol>
 			</div>
-			<div class="span6">
-				<!-- <img src="http://placehold.it/500x350"> -->
-
-				<div id="myCarousel" class="carousel slide">
-					<ol class="carousel-indicators">
-					    <li data-target="#myCarousel" data-slide-to="0"></li>
-					    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-					    <li data-target="#myCarousel" data-slide-to="2"></li>
-					    <li data-target="#myCarousel" data-slide-to="3"></li>
-					    <li data-target="#myCarousel" data-slide-to="4"></li>
-					</ol>
-
-					<!-- Carousel items -->  
-				    <div class="carousel-inner">  
-				    	<div class="item">
-				        	<img src="../../images/smartbite_observation.jpg" alt="First sketches" />
-				        	<div class="carousel-caption">
-				        		<p>Una de las personas observadas para encontrar una oportunidad de diseño.</p>
-				        	</div>
-				        </div>  
-				        <div class="item active">
-				        	<img src="../../images/smartbite_storyboard.png" alt="Storyboard" />
-				        	<div class="carousel-caption">
-				        		<p>Storyboard para representar el contexto donde la app sería usada.</p>
-				        	</div>
-				        </div>  
-				        
-				        <div class="item">
-				        	<img src="../../images/smartbite_lowfidelity2.png" alt="" />
-				        	<div class="carousel-caption">
-				        		<p>Prototipo básico construído usando Balsamiq Mockups</p>
-				        	</div>
-				        </div>
-				        <div class="item">
-				        	<img src="../../images/smartbite_highfidelity.png" alt="" />
-				        	<div class="carousel-caption">
-				        		<p>Prototipo más detallado teniendo en cuenta el feedback de los usuarios, construído con Justinmind</p>
-				        	</div>
-				        </div>    
-				        <div class="item">
-				        	<img src="../../images/smartbite_usertest.jpg" alt="" />
-				        	<div class="carousel-caption">
-				        		<p>Prueba de Usuario</p>
-				        	</div>
-				        </div>  
-				  	</div>  
-				  	<!-- Carousel nav -->  
-				  	<a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a>  
-				  	<a class="carousel-control right" href="#myCarousel" data-slide="next">›</a>  
-				</div>  
-
-
-			</div>
 		</div>
+		<!-- </div> -->
 
 		<hr/>
 
 		<!-- Tools Used -->
 		<div class="row">
 			<div class="span12">
-				<h3>Herramientas usadas</h3>
+				<h3 style="color:gray">Herramientas usadas</h3>
 			</div>
 			<div class="offset4 span4">
 				<!-- <img src="http://placehold.it/700x150"> -->
@@ -151,7 +143,7 @@
 
 		<div class="row">
 			<div class="span12">
-				<h3>Detalles</h3>
+				<h3 style="color:gray">Detalles</h3>
 			</div>
 			<div class="span12">
 				<dl class="dl-horizontal">
