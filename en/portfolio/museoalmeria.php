68986cfd7fe48648e875a86ece230aff079c5a78
Jose Viso <jvisov@gmail.com>
2013-07-08 10:18:27 +0200

    museoalmeria: Updated both versions

--- en/portfolio/museoalmeria.php
+++ en/portfolio/museoalmeria.php
@@ -2,78 +2,175 @@
 
 	$page_title = "Portfolio > Museum of Almería Kiosk";
 	$page_description = "Portfolio de Jose Viso | Interactive kiosk for the Museum of Almería";
-	$project_name = "Museum of Almería Kiosk";
-	$herounit_width = 7;
-	$herounit_desc = "<p>This project is a concept of interactive kiosk especially devised for the needs of the <a href='http://www.museosdeandalucia.es/culturaydeporte/museos/MAL/?lng=es'>Museum of Almería</a>. It was created for the Interaction Design class of the <a href='http://estudis.uoc.edu/ca/masters-postgraus-especialitzacions/diploma-postgrau/informatica-multimedia-telecomunicacio/interaccio-persona-ordinador/presentacio'>Postgraduate course on Human-Computer Interaction</a> of the Open University of Catalunya.</p>";
-	$image_front = "../../images/museoalmeria_front.png";
 	
-	$design_opportunity_desc = 
-		"<p>I made a visit to the museum to find not covered needs and design opportunities, and I got the following:</p>
-		 <ul>
-			<li><p>Very <b>few information about the</b> regular archeological <b>elements</b>. There were people asking themselves what were some of the objects on the showcases, and they didn't have where to go to resolve their doubts.</li>
-			<li><p>There wasn't a way for the users to <b>give feedback to the museum</b> to improve, not even an information point.</p></li>
-			<li><p>The temporary exhibition room had a lot of pieces of work, but almost no information about what they were or the authors (just a small piece of card with the name).</p></li>
-			<li><p>I missed a <b>guiding thread</b> to indicate the visitors why certain objects were located in particular zones.</p></li>
-		 </ul>";
-	
-	$process = array(
-	    	"steps" =>  array(
-				"Creation of <b>personas</b> to represent the 3 main roles detected.",
-				"Definition of the <b>information architecture</b>. First I made an outline and after that I created the site map.",
-				"Definition of the <b>interaction flows</b> in the application. I identified the main tasks the users needed to do and throught different charts I designed how the application should work.",
-				"Creation of <b>sketches and wireframes</b>. First I tried to get a good number of alternatives for each screen. After that I chose an option and continued developing it with a higher level of fidelity."
-			),
-			"slides" => array(
-				0 => array(
-						"image" => "../../images/museoAlmeria_personas.jpg",
-						"alt_text" => "Personas",
-						"caption" => "The three personas defined"
-					 ),
-				1 => array(
-						"image" => "../../images/museoAlmeria_IA.jpg",
-						"alt_text" => "Information Architecture",
-						"caption" => "Information Architecture of the kiosk"
-					 ),
-				2 => array(
-						"image" => "../../images/museoAlmeria_flowchart.jpg",
-						"alt_text" => "Flow charts",
-						"caption" => "Flow chart for one of the functions"
-					 ),
-				3 => array(
-						"image" => "../../images/museoAlmeria_prototype.jpg",
-						"alt_text" => "First prototype",
-						"caption" => "Home screen for the first prototype"
-					 ),
-				4 => array(
-						"image" => "../../images/museoAlmeria_mf-wireframe.jpg",
-						"alt_text" => "Mid-fidelity wireframe",
-						"caption" => "Annotated wireframe for the object information screen"
-					 ),
-				5 => array(
-						"image" => "../../images/museoalmeria_hf-wireframe.jpg",
-						"alt_text" => "High-fidelity wireframe",
-						"caption" => "High-fidelity wireframe for the home screen"
-					 )
-			),
-			"active" => 5
-	);
+	include("../includes/header.php"); ?>
 
-	$tools_used = array(
-		"Pen and Paper",
-		"Balsamiq Mockups",
-		"OmniGraffle",
-		"Photoshop"
-	);
+	<div class="row">
+		<div class="span12">
+			<ul class="breadcrumb">
+				<li><a href="<?php echo $base; ?>/en/">Home</a> <span class="divider">></span></li>
+				<li><a href="<?php echo $base; ?>/en/portfolio"> Portfolio</a> <span class="divider">></span></li>
+				<li class="active">Museum of Almería Kiosk</li>	
+		    </ul>
+		</div>
+	</div>
 
-	$details = array(
-		"date" => "March - May 2013",
-		"tags" => array(
-			"Interaction Design" => "interaction-design"
-		)
-	);
-	
-	include("../includes/header.php"); 
-	include("../includes/portfolio-template.php");
-	include("../includes/footer.php"); 
+		<!-- Big Text -->	
+		<div class="header"><h1>Museum of Almería Kiosk</h1></div>
+		
+		<!-- Summary -->
+		<div class="row spacer">
+			<div class="span12">
+				<div class="row">
+					<div class="span7">
+						<p class="lead">This project is a concept of interactive kiosk especially devised for the needs of the <a href='http://www.museosdeandalucia.es/culturaydeporte/museos/MAL/?lng=es'>Museum of Almería</a>.</p>
+						<p class="lead">It was created for the Interaction Design class of the <a href='http://estudis.uoc.edu/ca/masters-postgraus-especialitzacions/diploma-postgrau/informatica-multimedia-telecomunicacio/interaccio-persona-ordinador/presentacio'>Postgraduate course on Human-Computer Interaction</a> of the Open University of Catalunya.</p>
+					</div>
+					<div class="span5">
+						<div class="pull-right hidden-phone">
+							<img src="../../images/museoalmeria_front.png">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	?>
+		<hr/>
+
+		
+		<!-- Design Opportunity -->
+		<div class="row">
+			<div class="span12">
+				<h3 style="color:gray;">Design opportunity</h3>
+				<div class="row">
+					<div class="span7">
+						<p>I made a visit to the museum to find not covered needs and design opportunities, and I got the following:</p>
+						<ul>
+							<li><p>Very <b>few information about the</b> regular archeological <b>elements</b>. There were people asking themselves what were some of the objects on the showcases, and they didn't have where to go to resolve their doubts.</li>
+							<li><p>There wasn't a way for the users to <b>give feedback to the museum</b> to improve, not even an information point.</p></li>
+							<li><p>The temporary exhibition room had a lot of pieces of work, but almost no information about what they were or the authors (just a small piece of card with the name).</p></li>
+							<li><p>I missed a <b>guiding thread</b> to indicate the visitors why certain objects were located in particular zones.</p></li>
+						</ul>
+					</div>
+					<div class="span5">
+						<img class="pull-right" src="../../images/moleskine.jpg" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr/>
+
+		<!-- Design Process -->
+		<div class="row">
+			<div class="span12">
+				<h3 style="color:gray;">Process</h3>
+				<div class="row">
+					<div class="span7">
+						<ol>
+							<li>
+								<p class="spacer">Creation of <b>personas</b> to represent the 3 main roles detected.</p>
+								<div class="centered">
+									<img src="../../images/museoAlmeria_personas.png" alt="Personas" />
+								</div>
+							</li>
+
+							<li>
+								<p class="spacer">Definition of the <b>information architecture</b>. First I made an outline and after that I created the site map.</p>
+								<div class="centered">
+									
+
+												<a data-toggle="lightbox" href="#ia_big">
+													<img src="../../images/museoAlmeria_ia_small.png" class="img-polaroid" alt="Click to make bigger">
+												</a>
+												<div id="ia_big" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
+														<div class='lightbox-header'>
+															<button type="button" class="close" data-dismiss="lightbox" aria-hidden="true">&times;</button>
+														</div>
+														<div class='lightbox-content'>
+															<img src="../../images/museoAlmeria_ia.jpg">
+														</div>
+												</div>
+								</div>
+							</li>
+							<li>
+								<p class="spacer">Definition of the <b>interaction flows</b> in the application. I identified the main tasks the users needed to do and throught different charts I designed how the application should work.</p>
+								<div class="centered">
+									<img src="../../images/museoAlmeria_flowchart_small.png" alt="Flujos de interacción" />
+								</div>
+							</li>
+							<li>
+								<p class="spacer">Creation of <b>sketches and wireframes</b>. First I tried to get a good number of alternatives for each screen. After that I chose an option and continued developing it with a higher level of fidelity.</p>
+								<div class="centered">
+									<img src="../../images/museoAlmeria_sketches.JPG" alt="Sketch en papel" />
+									<p class="centered muted">First sketches of design alternatives for the home screen.</p>
+								</div>
+								<div class="spacer centered">
+									<img src="../../images/museoAlmeria_lowfi_wireframe.png" alt="Primer prototipo" />
+									<p class="centered muted">Once an alternative was picked up, I created a version with Balsamiq Mockups.</p>
+								</div>
+								<div class="spacer centered">
+												<a data-toggle="lightbox" href="#annotated_wireframe_big">
+													<img src="../../images/museoAlmeria_annotated_wireframe_small.jpg" class="img-polaroid" alt="Click to make bigger">
+												</a>
+												<p class="centered muted">Annotated wireframe explaining the "Detailed information about object" screen.</p>
+												<div id="annotated_wireframe_big" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
+														<div class='lightbox-header'>
+															<button type="button" class="close" data-dismiss="lightbox" aria-hidden="true">&times;</button>
+														</div>
+														<div class='lightbox-content'>
+															<img src="../../images/museoAlmeria_annotated_wireframe.jpg">
+														</div>
+												</div>
+
+									<img class="spacer" src="../../images/museoalmeria_hf_wireframe.jpg" alt="Wireframe de alta fidelidad" />
+									<p class="centered muted">High-fidelity version created using OmniGraffle.</p>
+								</div>
+							</li>
+						</ol>
+						</div>
+				</div>
+			</div>
+		</div>
+
+		<hr/>		
+
+		<!-- Tools Used -->
+		<div class="row">
+			<div class="span12">
+				<h3 style="color:gray;">Tools used</h3>
+			</div>
+			<div class="offset4 span4">
+				<!-- <img src="http://placehold.it/700x150"> -->
+				<table class="table table-bordered">
+					<tr>
+						<td><p class="centered">Pen and Paper</p></td>
+					</tr>
+					<tr>
+						<td><p class="centered">Balsamiq Mockups</p></td>
+					</tr>
+					<tr>
+						<td><p class="centered">OmniGraffle</p></td>
+					</tr>
+  				</table>
+			</div>
+		</div>
+
+		<hr/>
+
+		<div class="row">
+			<div class="span12">
+				<h3 style="color:gray;">Details</h3>
+			</div>
+			<div class="span12">
+				<dl class="dl-horizontal">
+				  <dt>Date</dt>
+				  <dd>March - May 2013</dd>
+				  <dt>Tags</dt>
+				  <dd>
+				      <a href="<?php echo $base . '/en/portfolio/category/interaction-design'; ?>"><span class="label">Interaction Design</span></a>
+				  </dd>
+				</dl>
+			</div>
+		</div>
+
+<?php include("../includes/footer.php"); ?>
