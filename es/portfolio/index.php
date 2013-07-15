c6312118db3f7c7c09acaa6083f82f5c3dcc988e
Jose Viso <jvisov@gmail.com>
2013-07-15 20:27:26 +0200

    Portfolio: Removed project name and add it on hover

--- es/portfolio/index.php
+++ es/portfolio/index.php
@@ -6,15 +6,12 @@
  	include("../includes/header.php");
  	include("../includes/portfolio-subnav.php"); ?>
 
- 	
-
 	<!-- Projects -->		  
 	<ul class="thumbnails">
 		<?php foreach ($projects as $url => $name): ?>
 			<li class="span4 spacer">   
 				<a href="<?php echo $url ?>" class="thumbnail">
-			    	<img src="../../images/thumb_<?php echo $url; ?>.png" alt="">
-			    	<h3 class="centered"><?php echo $projects[$url]; ?></h3>
+			    	<img src="../../images/thumb_<?php echo $url; ?>.png" onmouseover="this.src='<?php echo $base; ?>/images/thumb_bg_<?php echo $url; ?>.png'" onmouseout="this.src='<?php echo $base; ?>/images/thumb_<?php echo $url; ?>.png'">
 			    </a>
 			</li>
 		<?php endforeach; ?>
