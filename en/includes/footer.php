e98a38600821588657fdd39976464620f88af6e9
Jose Viso <jvisov@gmail.com>
2013-07-17 17:16:00 +0200

    footer: Reduced the description text for the social networks

--- en/includes/footer.php
+++ en/includes/footer.php
@@ -6,9 +6,9 @@
 	<footer>
 		<div class="container">
 			<p class="muted credit">
-				<a href="mailto:contact@joseviso.com"><img src="../../images/icons/PNG/mail.png" onmouseover="this.src='../../images/icons/PNG/mail-hover.png'" onmouseout="this.src='../../images/icons/PNG/mail.png'"/></a>&nbsp;
-				<a href="http://facebook.com/joseviso" title="My Facebook page"><img src="../../images/icons/PNG/facebook-32.png" onmouseover="this.src='../../images/icons/PNG/facebook-hover.png'" onmouseout="this.src='../../images/icons/PNG/facebook-32.png'"/></a>&nbsp;
-				<a href="http://twitter.com/XoseViso" title="My Twitter account"><img src="../../images/icons/PNG/twitter-32.png" onmouseover="this.src='../../images/icons/PNG/twitter-hover.png'" onmouseout="this.src='../../images/icons/PNG/twitter-32.png'"/></a>
+				<a href="mailto:contact@joseviso.com"><img src="<?php echo $base;?>/images/icons/PNG/mail.png" onmouseover="this.src='<?php echo $base;?>/images/icons/PNG/mail-hover.png'" onmouseout="this.src='<?php echo $base;?>/images/icons/PNG/mail.png'"/></a>&nbsp;
+				<a href="http://facebook.com/joseviso" title="My Facebook page"><img src="<?php echo $base;?>/images/icons/PNG/facebook-32.png" onmouseover="this.src='<?php echo $base;?>/images/icons/PNG/facebook-hover.png'" onmouseout="this.src='<?php echo $base;?>/images/icons/PNG/facebook-32.png'"/></a>&nbsp;
+				<a href="http://twitter.com/XoseViso" title="My Twitter account"><img src="<?php echo $base;?>/images/icons/PNG/twitter-32.png" onmouseover="this.src='<?php echo $base;?>/images/icons/PNG/twitter-hover.png'" onmouseout="this.src='<?php echo $base;?>/images/icons/PNG/twitter-32.png'"/></a>
 			</p>
 		</div>
 	</footer>
