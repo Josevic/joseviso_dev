33dafbfc0ca1fea26a0db04ef3a37efc2e509d7c
Jose Viso <jvisov@gmail.com>
2013-08-04 22:44:48 +0200

    Improved the about page

--- en/about.php
+++ en/about.php
@@ -18,15 +18,18 @@
 			<div class="row">
 				<div class="offset1 span4">
 					<p class="lead">José Viso Vargas</p>
-					<p><em>User Experience Designer / Developer</em></p>
+					<p><em>User Experience Designer & Developer</em></p>
 					<p>September 29th, 1984 (28 years old)</p>
-					<p>Living in Almería, Spain</p>
+					<p>From Almería (Spain)</p>
+					
+					<!--
 					<p>Studying a <a href="http://www.uoc.edu/masters/esp/web/informatica_multimedia_telecomunicacion/interaccion-persona-ordenador/">Human-Computer Interaction Postgraduate degree</a><br/>
 					   <em class="muted">(Universitat Oberta de Catalunya)</em>
 					</p> 
 					<p>Bachelor of Computer Science<br/>
 					   <em class="muted">(Universidad de Almería)</em>
 					</p>
+					-->
 				</div>
 				<div class="span4">
 					<img src="../images/jose_about.jpg" alt="Photo of Jose Viso"/>
@@ -40,29 +43,28 @@
 							<div class="span8">
 								<!-- Explanation of what I do -->
 								<p class="lead">User Centered Design</p>
-								<p>My main goal is to <strong>design software that satisfy the users' needs</strong>, studying their capabilities, motivations and limitations, but considering the client's needs too.</p>
-								<p>I've worked in complex projects, both in Spain and abroad, analyzing the user requirements and developing software. Later I've specialized in the Human-Computer Interaction field, that is what I'm really passionate about. I think this experience helps me to have a global vision for the projects I work on. 
-						   		   You can find some of the things I've made taking a look at <a href="portfolio">my portfolio</a>.</p>
+								<p>I <strong>design and build software that satisfy the users' needs</strong>, studying their capabilities, motivations and limitations, but considering the client's needs too.</p>
+								<p>I graduated in Computer Science, and after that I've been working in complex projects, both in Spain and abroad, from analyzing the user requirements to deploying software.</p>
+								<p>Later I've specialized in Human-Computer Interaction, that is what I'm really passionate about.</p>
+								<p>You can find some of the things I've made taking a look at <a href="portfolio">my portfolio</a>.</p>
 							</div>
 						</div>
 						<div class="row">
 							<div class="span8">
 								<h4 class="spacer">Techniques, Methodologies and Tools I use</h4>
 								<ol>
-									<li>Sketchboard</li>
-									<li>Heuristic Evaluations and other Usability studies</li>
-									<li>User Research</li>
-									<li>Personas</li>
-									<li>Scenarios</li>
-									<li>Storyboarding</li>
-									<li>Sitemap diagrams</li>
-									<li>User flow diagrams</li>
-									<li>Prototyping: paper prototypes, low and high fidelity graphic tools (Balsamiq Mockups, Axure, Justinmind), code prototypes.</li>
-									<li>Wireframing (OmniGraffle, Fireworks, Photoshop)</li>
-									<li>User tests</li>
-									<li>Iterative Design: (Design - Prototype - Evaluate)</li>
-									<li>Agile Development</li>
-									<li>Software frameworks and CMSs</li>
+									<li>Iterative Design <small class="muted">(Design - Prototype - Evaluate)</small>.</li>
+										<ul>
+											<li>Personas, Scenarios and Storyboarding.</li>
+											<li>Sitemap diagrams and user flow diagrams.</li>
+											<li>Prototyping: paper prototypes, low and high fidelity graphic tools <small class="muted">(Balsamiq Mockups, Axure, Justinmind)</small>, code prototypes.</li>
+											<li>Wireframing <small class="muted">(OmniGraffle, Fireworks, Photoshop)</small>.</li>
+											<li>User Testing <small class="muted">(Silverback)</small>.</li>
+										</ul>
+									
+									<li>Heuristic evaluations and other usability studies.</li>
+									<li>Agile Development.</li>
+									<li>Software frameworks and CMSs <small class="muted">(CodeIgniter, Twitter Bootstrap, Wordpress, etc)</small>.</li>
 								</ol>
 							</div>
 
@@ -77,15 +79,15 @@
 						<div class="row">
 							<div class="span8">
 								<ul>
-									<li><em>Community TA</em> in the <a href="https://www.coursera.org/course/hci">Human-Computer Interaction course</a> by <a href="https://www.coursera.org/">Coursera</a> - Stanford University</li>
-									<li>Founder of <a href="http://streetrockstars.com">Street Rockstars</a></li>
+									<li><em>Community TA</em> in the <a href="https://www.coursera.org/course/hci">Human-Computer Interaction course</a> by <a href="https://www.coursera.org/">Coursera</a> - Stanford University.</li>
+									<li>Founder of <a href="http://streetrockstars.com">Street Rockstars</a>.</li>
 								</ul>
 
 								<h4 class="spacer">Experience</h4>
 								<ul>
-									<li>User Experience Designer / Developer (Almería, Spain - International)</li>
-									<li>Web Developer (Amsterdam, Netherlands)</li>
-									<li>Backend Developer (Almería, Spain)</li>
+									<li>User Experience Designer & Developer <small class="muted">Apr 2012 - Present (Almería, Spain)</small>.</li>
+									<li>Web Developer <small class="muted">Apr 2011 - Apr 2012 (Amsterdam, Netherlands)</small>.</li>
+									<li>Backend Developer <small class="muted">May 2008 - Apr 2011 (Almería, Spain)</small>.</li>
 								</ul>
 							</div>
 						</div>
@@ -98,14 +100,13 @@
 				<h3 class="spacer">Things I like</h3>
 				<div class="row">
 					<div class="offset1 span8">			
-						<p>Sports, Photography, Music, Science, To travel</p>
-						<p class="spacer">One quote that summarizes my way of thinking is:</p>
+						<p>Sports, Photography, Music, Science, To travel, Food.</p>
+
 						<blockquote class="spacer">
 							<p>Design should not dominate things. Not dominate people. It should help people.</p>
 							<small>Dieter Rams</small>
 						</blockquote>
 					</div>
-
 				</div>
 			
 			</div>
