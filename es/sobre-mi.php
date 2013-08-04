67de500232a592f1d370479952356e14c03553e5
Jose Viso <jvisov@gmail.com>
2013-08-04 23:25:53 +0200

    Improved the about page

--- es/sobre-mi.php
+++ es/sobre-mi.php
@@ -18,17 +18,9 @@
 			<div class="row">
 				<div class="offset1 span4">
 					<p class="lead">José Viso Vargas</p>
-					<p><em>Diseñador / Desarrollador de Experiencia de Usuario</em></p>
+					<p><em>Diseñador de Experiencia de Usuario y Desarrollador</em></p>
 					<p>29 Septiembre 1984 (28 años)</p>
-					<p>Almería, España</p>
-					<p>
-						Cursando <a href="http://www.uoc.edu/masters/esp/web/informatica_multimedia_telecomunicacion/interaccion-persona-ordenador/">Posgrado de Interacción Persona-Ordenador</a><br>
-					   <em class="muted">(Universitat Oberta de Catalunya)</em>
-					</p> 
-					<p>
-					   Ingeniero Técnico en Informática de Gestión<br/>
-					   <em class="muted">(Universidad de Almería)</em>
-					</p>
+					<p>De Almería (España)</p>
 				</div>
 				<div class="span4">
 					<img src="../images/jose_about.JPG" alt="Foto de Jose Viso"/>
@@ -43,30 +35,28 @@
 							<div class="span8">
 								<!-- Explanation of what I do -->
 								<p class="lead">Diseño Centrado en el Usuario </p>
-								<p>Mi principal objetivo es <b>diseñar software que satisfaga las necesidades de los usuarios</b>, estudiando sus capacidades, motivaciones y limitaciones, pero teniendo en cuenta también las necesidades del cliente.</p>
-								<p>He trabajado en proyectos complejos tanto en España como en el extranjero, analizando los requisitos de los usuarios y desarrollando software. Más tarde me he especializado en el campo de la Interacción Persona-Ordenador, que es lo que realmente me apasiona. Creo que esta experiencia me permite poseer una buena visión global de los proyectos.
-								Puedes encontrar algunas cosas de las que he hecho echando un vistazo a <a href="portfolio">mi portfolio</a>.</p>
+								<p>Me dedico a <b>diseñar software que satisfaga las necesidades de los usuarios</b>, estudiando sus capacidades, motivaciones y limitaciones, pero teniendo en cuenta también las necesidades del cliente.</p>
+								<p>Estudié Ingeniería Técnica en Informática de Gestión y desde entonces he estado trabajado en proyectos complejos tanto en España como en el extranjero, desde el análisis de los requisitos de usuario hasta la puesta en producción.</p>
+								<p>Más tarde me he especializado en el campo de la Interacción Persona-Ordenador, que es lo que realmente me apasiona.
+								<p>Puedes encontrar algunas cosas de las que he hecho echando un vistazo a <a href="portfolio">mi portfolio</a>.</p>
 							</div>
 						</div>
 						<div class="row">
 							<div class="span8">
 								<h4 class="spacer">Técnicas, Metodologías y Herramientas que utilizo</h4>
-							
 								<ol>
-									<li>Sketchboard</li>
-									<li>Evaluacines Heurísticas y otros estudios de Usabilidad</li>
-									<li>Investigación de Usuarios</li>
-									<li>Personas</li>
-									<li>Escenarios</li>
-									<li>Creación de Storyboards</li>
-									<li>Árboles de contenidos o mapas del sitio</li>
-									<li>Diagramas de flujo de interacción</li>
-									<li>Prototipado: prototipos en papel, herramientas gráficas de baja y alta fidelidad (Balsamiq Mockups, Axure, Justinmind), protitipos en código.</li>
-									<li>Creación de Wireframes (OmniGraffle, Fireworks, Photoshop)</li>
-									<li>Pruebas de Usuario</li>
-									<li>Diseño Iterativo (Diseñar - Prototipar - Evaluar)</li>
-									<li>Desarrollo Ágil</li>
-									<li>Frameworks software y CMSs</li>
+									<li>Diseño Iterativo <small class="muted">(Diseñar - Prototipar - Evaluar)</small>.</li>
+									<ul>
+										<li>Personas, Escenarios y Storyboards.</li>
+										<li>Árboles de contenidos o sitemaps y Diagramas de flujo de interacción.</li>
+										<li>Prototipado: prototipos en papel, herramientas gráficas de baja y alta fidelidad (Balsamiq Mockups, Axure, Justinmind), prototipos en código.</li>
+										<li>Creación de Wireframes <small class="muted">(OmniGraffle, Fireworks, Photoshop)</small>.</li>
+										<li>Pruebas de Usuario <small class="muted">(Silverback)</small>.</li>
+									</ul>
+					
+									<li>Evaluacines Heurísticas y otros estudios de Usabilidad.</li>
+									<li>Desarrollo Ágil.</li>
+									<li>Frameworks software y CMSs <small class="muted">(CodeIgniter, Twitter Bootstrap, Wordpress, etc)</small>.</li>
 								</ol>
 							</div>
 							<div class="span8">
@@ -78,15 +68,15 @@
 						<div class="row">
 							<div class="span8">
 								<ul>
-									<li><em>Community TA</em> en el <a href="https://www.coursera.org/course/hci">curso de Interacción Persona-Ordenador</a> de <a href="https://www.coursera.org/">Coursera</a> - Stanford University</li>
-									<li>Creador de <a href="http://streetrockstars.com">Street Rockstars</a></li>
+									<li><em>Community TA</em> en el <a href="https://www.coursera.org/course/hci">curso de Interacción Persona-Ordenador</a> de <a href="https://www.coursera.org/">Coursera</a> - Stanford University.</li>
+									<li>Creador de <a href="http://streetrockstars.com">Street Rockstars</a>.</li>
 								</ul>
 
 								<h4 class="spacer">Experiencia</h4>
 								<ul>
-									<li>Consultor de Experiencia de Usuario (Almería, España - Internacional)</li>
-									<li>Desarrollador web (Ámsterdam, Países Bajos)</li>
-									<li>Desarrollador backend (Almería, España)</li>
+									<li>Consultor de Experiencia de Usuario <small class="muted">Abr 2012 - Presente (Almería, España)</small>.</li>
+									<li>Desarrollador web <small class="muted">Abr 2011 - Abr 2012 (Ámsterdam, Países Bajos)</small>.</li>
+									<li>Desarrollador backend <small class="muted">May 2008 - Abr 2011 (Almería, España)</small>.</li>
 								</ul>
 							</div>
 						</div>
@@ -97,8 +87,8 @@
 			<h3 class="spacer">Cosas que me gustan</h3>
 				<div class="row">
 					<div class="offset1 span8">
-						<p>Deportes, Fotografía, Música, Ciencia, Viajar</p>
-						<p class="spacer">Una frase que representa muy bien mi filosofía es esta:</p>
+						<p>Deportes, Fotografía, Música, Ciencia, Viajar, Comida.</p>
+						
 						<blockquote class="spacer">
 							<p>El diseño no debería dominar a las cosas. Ni dominar a la gente. Debería ayudar a la gente.</p>
 							<small>Dieter Rams</small>
